b65a89b9678027bd35b21d76a92f7a576caf3b44
sergezhu <zs.inkvizitor@gmail.com>
2022-08-31 16:41:44 +0300

    internal types, internal utils

---
+++ Assets/Game/Code/Core/InternalTypes/Vector3Internal.cs
@@ -0,0 +1,25 @@
+﻿namespace Game.Code.Core.InternalTypes
+{
+	using System;
+
+	[Serializable]
+	public class Vector3Internal
+	{
+		public float X;
+		public float Y;
+		public float Z;
+
+		public Vector3Internal( float x, float y, float z )
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+
+		public static Vector3Internal operator +( Vector3Internal a, Vector3Internal b )
+			=> new Vector3Internal( a.X + b.X, a.Y + b.Y, a.Z + b.Z );
+
+		public static Vector3Internal operator -( Vector3Internal a, Vector3Internal b )
+			=> new Vector3Internal( a.X - b.X, a.Y - b.Y, a.Z - b.Z );
+	}
+}
---
+++ Assets/Game/Code/Core/InternalUtils/Math.cs
@@ -0,0 +1,14 @@
+﻿namespace Game.Code.Core.InternalUtils
+{
+	using UnityEngine;
+
+	public static class Math
+	{
+		// Here may be funcs implementations not from UnityEngine
+		
+		public static float Clamp( float f, float a, float b ) => Mathf.Clamp( f, a, b );
+		public static float Abs( float f ) => Mathf.Abs( f );
+		public static float Sin( float radians ) => Mathf.Sin( radians );
+		public static float Cos( float radians ) => Mathf.Cos( radians );
+	}
+}
