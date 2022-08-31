08e0d9b7a9c04725c455bb6879051c3c8d780a64
sergezhu <zs.inkvizitor@gmail.com>
2022-08-31 14:22:35 +0300

    InputTest

--- Assets/Game/Code/Unity/Bootstrap/Bootstrapper.cs
+++ Assets/Game/Code/Unity/Bootstrap/Bootstrapper.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 
 public class Bootstrapper : MonoBehaviour
 {
+	private InputTest _inputTest;
+	
 	private IInputManager _inputManager;
 	private MouseAndKeyboardControl _mouseAndKeyboardControl;
 
-	private void Start()
+	private void Awake()
 	{
 		_inputManager            = new InputManager();
 		_mouseAndKeyboardControl = new MouseAndKeyboardControl( _inputManager );
+
+		_inputTest = FindObjectOfType<InputTest>();
+		_inputTest.Init( _mouseAndKeyboardControl );
 	}
 }
---
+++ Assets/Game/Code/Unity/Input/InputTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Game.Code.Unity.Input
+{
+	using UnityEngine;
+
+	public class InputTest : MonoBehaviour
+	{
+		private MouseAndKeyboardControl _mouseAndKeyboardControl;
+
+		public void Init( MouseAndKeyboardControl mouseAndKeyboardControl )
+		{
+			_mouseAndKeyboardControl = mouseAndKeyboardControl;
+
+			Subscribe();
+		}
+
+		private void Subscribe()
+		{
+			_mouseAndKeyboardControl.Move += OnMove;
+			_mouseAndKeyboardControl.RotateCW += OnRotateCW;
+			_mouseAndKeyboardControl.RotateCCW += OnRotateCCW;
+		}
+
+		private void OnMove() => Debug.Log( "Move" );
+		private void OnRotateCW() => Debug.Log( "RotateCW" );
+		private void OnRotateCCW() => Debug.Log( "RotateCCW" );
+	}
+}
