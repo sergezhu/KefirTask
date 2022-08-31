6bbb0fca23496bb92ce0fa72b6e074e0a3e85c2b
sergezhu <zs.inkvizitor@gmail.com>
2022-09-01 00:03:57 +0300

    Rotator

---
+++ Assets/Game/Code/Core/Move/Rotator.cs
@@ -0,0 +1,26 @@
+﻿namespace Game.Code.Core.Move
+{
+	using System.Numerics;
+
+	public class Rotator
+	{
+		private readonly float _rotationSpeed;
+		private readonly Quaternion _rotationQuaternionSpeed;
+		private readonly Quaternion _delta;
+
+		public Quaternion CurrentRotation { get; private set; }
+
+		public Rotator(Quaternion startRotation, Quaternion delta, float rotationSpeed )
+		{
+			CurrentRotation = startRotation;
+			_rotationSpeed = rotationSpeed;
+			_delta         = delta;
+		}
+
+		public void Tick( float deltaTime )
+		{
+			var desiredRotation = _delta * CurrentRotation;
+			CurrentRotation = Quaternion.Slerp( CurrentRotation, desiredRotation, deltaTime * _rotationSpeed );
+		}
+	}
+}
