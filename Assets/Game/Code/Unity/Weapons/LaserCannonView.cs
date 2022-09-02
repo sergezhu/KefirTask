f29a9312d0339d2b5b2b6defdbbcd08091951cb0
sergezhu <zs.inkvizitor@gmail.com>
2022-09-03 00:24:08 +0300

    laser fade

--- Assets/Game/Code/Unity/Weapons/LaserCannonView.cs
+++ Assets/Game/Code/Unity/Weapons/LaserCannonView.cs
@@ -1,5 +1,6 @@
 ﻿namespace Game.Code.Unity.Weapons
 {
+	using System;
 	using System.Collections;
 	using UnityEngine;
 
@@ -9,12 +10,26 @@
 		[SerializeField] private LineRenderer _ray;
 
 		private Coroutine _coroutine;
+		private float _lightTimerStart;
+		private float _lightTimerEnd;
+		private Vector4 _emissionDefaultColor;
+		private Material _newMaterial;
 
 		public Transform ShootPoint => _shootPoint;
 
 		private void Awake()
 		{
-			HideLaser();
+			_newMaterial  = new Material( _ray.material );
+			_ray.material = _newMaterial;
+			
+			_emissionDefaultColor = _newMaterial.GetVector( "_EmissionColor" );
+
+			HideLaserInternal();
+		}
+
+		private void Update()
+		{
+			UpdateLightIntensity();
 		}
 
 		public void ShowLaser( float duration )
@@ -32,7 +47,7 @@
 
 		private IEnumerator ShowLaserRoutine( float duration )
 		{
-			ShowLaser();
+			ShowLaserInternal( duration );
 
 			var waiter = duration > 0.01f
 				? new WaitForSeconds( duration )
@@ -40,17 +55,33 @@
 			
 			yield return waiter;
 			
-			HideLaser();
+			HideLaserInternal();
 		}
 
-		private void ShowLaser()
+		private void ShowLaserInternal( float duration )
 		{
 			_ray.enabled = true;
+
+			_lightTimerStart = Time.time;
+			_lightTimerEnd   = _lightTimerStart + duration;
 		}
 
-		private void HideLaser()
+		private void HideLaserInternal()
 		{
 			_ray.enabled = false;
+		}
+
+		private void UpdateLightIntensity()
+		{
+			var t = Time.time;
+			var canUpdate = t >= _lightTimerStart && t <= _lightTimerEnd;
+
+			if ( !canUpdate )
+				return;
+
+			var relativeIntensity = (_lightTimerEnd - t) / (_lightTimerEnd - _lightTimerStart);
+
+			_newMaterial.SetVector( "_EmissionColor", _emissionDefaultColor * relativeIntensity );
 		}
 	}
 }
