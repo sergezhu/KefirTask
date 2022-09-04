8db2149fddf6cd9fc26b345d9051b9dadf8f9203
sergezhu <zs.inkvizitor@gmail.com>
2022-09-04 22:57:35 +0300

    LaserCharge, RecallTimer

---
+++ Assets/Game/Code/Unity/Weapons/LaserCharge.cs
@@ -0,0 +1,24 @@
+﻿namespace Game.Code.Unity.Weapons
+{
+	public class LaserCharge
+	{
+		private readonly float _recallDelay;
+		
+		private RecallTimer _recallTimer;
+
+		public float RecallProgress => _recallTimer.Progress;
+		public bool IsReady => _recallTimer.IsReady;
+
+		public LaserCharge( float recallDelay )
+		{
+			_recallDelay = recallDelay;
+		}
+
+		public void Tick( float deltaTime )
+		{
+			_recallTimer.Tick( deltaTime );
+		}
+
+		public void Recall() => _recallTimer.Recall();
+	}
+}
