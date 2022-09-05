03d9c8c736555289f7dc31c4e053248fbd07bae7
sergezhu <zs.inkvizitor@gmail.com>
2022-09-05 03:34:23 +0300

    LaserChargeBlockView

---
+++ Assets/Game/Code/Unity/UI/LaserChargeBlockView.cs
@@ -0,0 +1,12 @@
+﻿namespace Game.Code.Unity.UI
+{
+	using TMPro;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public class LaserChargeBlockView : MonoBehaviour
+	{
+		[SerializeField] private Image _progressImage;
+		[SerializeField] private TMP_Text _infoText;
+	}
+}
